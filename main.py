0d9045320123b17e1a9880960e894d01d849214f
Jesaya Fernando Napitupulu <jesayafernando9@gmail.com>
2024-07-10 18:36:03 +0700

    add tool to integrate openweather api for fetch coordinate and weather info and API.bible for randow bible's verse(s)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import logging, os, secrets, pymongo
+import logging, os, secrets, pymongo, requests, random, re
 
 from datetime import datetime
 
@@ -28,39 +28,52 @@ token = os.getenv("BOT_TOKEN")
 mongodb = os.getenv("MONGODB_URI")
 clientMongo = MongoClient(mongodb, server_api=ServerApi('1'))
 db = clientMongo['testing']
-collection = db['sessions'] 
+collection = db['sessions']
+
+#external api
+bible_api_key = os.getenv('BIBLE_API_KEY')
+openweather_api_key = os.getenv('OPENWEATHER_API_KEY')
+
+#===========================================================================================#
+
+VERSES = [
+  'JER.29.11',
+  '1CO.4.4-8',
+  'PHP.4.13',
+  'JHN.3.16',
+  'ROM.8.28',
+  'ISA.41.10',
+  'PSA.46.1',
+  'GAL.5.22-23',
+  'HEB.11.1',
+  '2TI.1.7',
+  '1CO.10.13',
+  'PRO.22.6',
+  'ISA.40.31',
+  'JOS.1.9',
+  'HEB.12.2',
+  'MAT.11.28',
+  'ROM.10.9-10',
+  'ROM.8.28',
+  'PHP.2.3-4',
+  'MAT.5.43-44',
+]
+
+
+bible_params = {
+    "content-type": "text",
+    "include-notes": "false",
+    "include-titles": "false",
+    "include-chapter-numbers": "false",
+    "include-verse-numbers": "true",
+    "include-verse-spans": "false",
+    "use-org-id": "false"
+}
 
 
 #===========================================================================================#
 
-def add(a: float, b: float):
-    """returns a + b."""
-    return a + b
-
-
-def subtract(a: float, b: float):
-    """returns a - b."""
-    return a - b
-
-
-def multiply(a: float, b: float):
-    """returns a * b."""
-    return a * b
-
-
-def divide(a: float, b: float):
-    """returns a / b."""
-    return a / b
-
-
-model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash", tools=[add, subtract, multiply, divide]
-)
-
-
-
-#===========================================================================================#    
-
+#===========================================================================================#
 class ArrayWithPartsAndRoles:
     def __init__(self):
         self.parts = []
@@ -84,6 +97,195 @@ class ArrayWithPartsAndRoles:
         return str(parts_roles)
 
 
+def parse_bible_references(reference):
+    match = re.match(r'([A-Z0-9]+)\.(\d+)\.(\d+)-(\d+)', reference)
+    if match:
+        book, chapter, start_verse, end_verse = match.groups()
+        start_verse = int(start_verse)
+        end_verse = int(end_verse)
+        return [f"{book}.{chapter}.{verse}" for verse in range(start_verse, end_verse + 1)]
+    else:
+        return [reference]
+    
+def increment_bible_reference(verse_reference):
+    # Regular expression to match the verse reference pattern like 'Isa 40:10'
+    pattern = r'([a-zA-Z]+) (\d+):(\d+)(?:-(\d+))?'
+    match = re.match(pattern, verse_reference)
+    
+    if match:
+        book = match.group(1)
+        chapter = int(match.group(2))
+        start_verse = int(match.group(3))
+        end_verse = int(match.group(4)) if match.group(4) else start_verse
+        
+        # Increment the end verse by 1
+        end_verse += 1
+        
+        if start_verse == end_verse:
+            return f"{book} {chapter}:{start_verse}"
+        else:
+            return f"{book} {chapter}:{start_verse}-{end_verse}"
+    else:
+        return "Invalid verse reference format"
+
+#===========================================================================================#
+
+def add(a: float, b: float):
+    """returns a + b."""
+    return a + b
+
+
+def subtract(a: float, b: float):
+    """returns a - b."""
+    return a - b
+
+
+def multiply(a: float, b: float):
+    """returns a * b."""
+    return a * b
+
+
+def divide(a: float, b: float):
+    """returns a / b."""
+    return a / b
+
+def joke():
+    """Fetches a dad joke from the API and returns it."""
+    headers = {'Accept': 'text/plain',}
+    url = f"https://icanhazdadjoke.com/"
+    api_response = requests.get(url,headers=headers)
+    return api_response.text
+
+
+def bible_verse():
+    """Fetches a random and selected verse from the bible API and returns it."""
+    selected_verses = random.choice(VERSES)
+    array_selected_verse = parse_bible_references(selected_verses)
+    
+    headers = {
+        'Accept': 'application/json',
+        'api-key': bible_api_key,
+        }
+    
+    reference = ""
+    combined_text = ""
+
+    for i, verse in enumerate(array_selected_verse):
+        api_url = "https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/verses/" + verse
+        response = requests.get(api_url, params=bible_params, headers=headers)
+        if response.status_code == 200:
+            data = response.json()['data']
+            if i == 0:
+                book, chapter, first_verse = re.match(r'([A-Z]+)\.(\d+)\.(\d+)', verse).groups()
+                reference = f"{book.capitalize()} {chapter}:{first_verse}"
+    
+                combined_text += data['content'].strip()
+            else:
+                reference = increment_bible_reference(reference)
+                combined_text += f"[{data['verseCount']}] {data['content'].strip()}"
+        else:
+            return None
+    # Removing unnecessary verse count indicators like [1] but keeping verse numbers like [11]
+    combined_text = re.sub(r'\[(\d+)\]', lambda m: m.group(1) if int(m.group(1)) > 1 else '', combined_text).strip()
+
+    bible_version = "KJV"
+    # print(f"{reference} {combined_text} {bible_version}")
+    return reference, combined_text, bible_version
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # for verse in array_selected_verses:
+    #     api_url =  "https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/verses/" + verse
+    #     response = requests.get(api_url, params=bible_params, headers=headers)
+    #     print(api_url)
+    #     if response.status_code == 200:
+    #         data = response.json()['data']
+    #         if not reference:
+    #             book, chapter, verse_num = re.match(r'([A-Z]+)\.(\d+)\.(\d+)', verse).groups()
+    #             reference = f"{book.capitalize()} {chapter}:{verse_num}"
+    #         text = data['content'].strip()
+    #         combined_text += text + " "
+    #     else:
+    #         return None
+    
+    # combined_text = combined_text.strip()
+    # bible_version = "KJV"
+    # print(reference, combined_text, bible_version)
+    # return reference, combined_text, bible_version
+    # api_url =  "https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/verses/"+selected_verse
+    # response = requests.get(api_url,params=bible_params,headers=headers)
+    # if response.status_code == 200:
+    #     data = response.json()['data']
+    #     reference = data['reference']
+    #     text = data['content'].strip()
+    #     bible_version = "KJV"
+    #     print(reference, text, bible_version)
+    # else:
+    #     return None
+
+def city_coordinates(city: str):
+    """Fetch city coordinates for get weather information or just get coordinate of city."""
+    params = {
+        "q"   : city,
+        "limit": 1,
+        "appid": openweather_api_key,
+    }
+    
+    api_url = "http://api.openweathermap.org/geo/1.0/direct"
+    response = requests.get(api_url, params=params)
+    if response.status_code == 200:
+        data =  response.json()
+        latitude = data[0]["lat"]
+        longitude = data[0]["lon"]
+        return latitude, longitude
+    else:
+        return None
+    
+def city_weather(lat: float, lon: float):
+    """Fetch weather information based on coordinates. 
+    Units will be used is metric, celcius for temperature and meter per second for speed.
+    Milimeter will be used for rain and snow measurements."""
+    api_url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "units": "metric",
+        "appid": openweather_api_key,
+    }
+    response = requests.get(api_url, params=params)
+    
+    if response.status_code == 200:
+        data =  response.json()
+        weather = data["weather"][0]["main"]
+        temperature = {
+            "current": data["main"]["temp"],
+            "feels_like": data["main"]["feels_like"],
+        }
+        wind_speed = data["wind"]["speed"]
+        return weather, temperature, wind_speed
+
+    else:
+        return None
+
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-pro", tools=[add, subtract, multiply, divide, joke, bible_verse, city_coordinates,city_weather]
+)
+
+
+
+#===========================================================================================#    
 def get_chat_history(session_id: str) -> list[dict]:
   """Retrieves the chat history for a given session ID."""
 
